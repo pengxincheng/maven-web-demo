6408b2ccb7cc3b91bf208f26211a724575c803e6
pengxincheng <pengxincheng@mininglamp.com>
2019-10-09 20:40:03 +0800

    init

---
+++ src/main/java/com/pxc/mavenwebdemo/MavenWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pxc.mavenwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenWebDemoApplication.class, args);
+    }
+
+}
